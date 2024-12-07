6bb80b40c653e5f2497a4394f45a05be0e37bfc3
Jon Esperanza <jonathanesperanza67@gmail.com>
2024-12-07 14:45:36 -0500

    qol improvement

--- advent_of_code/2024/python/util/timer.py
+++ advent_of_code/2024/python/util/timer.py
@@ -1,17 +1,21 @@
 import time
 
-""" Decorator to time a function and print the result """
+""" Decorator to time a function (milliseconds) and print the result """
 
 
 def timed(func, print_result=True):
     def wrapper(*args, **kwargs):
-        start = time.time()
+        start = current_milli_time()
         res = func(*args, **kwargs)
-        end = time.time()
+        end = current_milli_time()
         print(f"{func.__name__}")
         if print_result:
             print(f"\tResult: {res}")
-        print(f"\tTotal time taken: {end - start}")
+        print(f"\tTotal time taken: {end - start}ms")
         return res
 
     return wrapper
+
+
+def current_milli_time():
+    return round(time.time() * 1000)
